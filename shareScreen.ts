2537bda60b364852c5574a1a7599b70e0ffc88b9
Michel Gerding <MichelGerding@gmail.com>
2021-06-08 09:43:33 +0200

    versie 1

---
+++ shareScreen.ts
@@ -0,0 +1,232 @@
+import { xapi, DisplayPrompt, OutputConnectorConfiguration, OutputConnectorStatus } from './xapi';
+let xapi: xapi;
+
+const fallbackLanguage = 'dutch'; // languages: english, dutch
+const useMonitorName = true // monitor names are in variable connectornames
+
+const text = {
+    'english': {
+        shareToPrimaryScreenWarning: "the preview on this monitor will stop when a call has been connected.",
+        resetButtonMessage: "All previews have been stopped",
+        promptOptions: {
+            text: "To which screen do you want to share?",
+            title: "Share Screen",
+            option5: "Stop Preview"
+        }, 
+    },
+    'dutch': {
+        shareToPrimaryScreenWarning: 'het voorbeeld van deze invoer wordt verwijderd als een gesprek begonnen wordt.',
+        resetButtonMessage: "Alle voorbeelden zijn gestopt",
+        promptOptions: {
+            text: "Naar welk scherm wil je delen?",
+            title: "Scherm delen",
+            option5: "Stop delen"
+        }
+    },
+}
+
+const connectorNames = [
+    'display 1', // output connector 1
+    'display 2', // output connector 2
+    'display 3', // output connector 3
+]
+
+//////////////////// DO NOT ANYTHING EDIT PAST THIS //////////////////// 
+// all events 
+xapi.Event.CallDisconnect.on(callDisconnected)
+xapi.Status.Call.Status.on(callStatusChanged)
+xapi.Event.PresentationPreviewStarted.on(presentationPreviewStartEvent)
+xapi.Event.UserInterface.Extensions.Panel.Clicked.on(resetbuttonPressed); 
+
+// setup for the language 
+let language = fallbackLanguage;
+xapi.Config.UserInterface.Language.get().then((lang) => {
+    // initail run
+    if (text[lang.toLocaleLowerCase()]) {
+        language = lang.toLowerCase()
+    }
+})
+xapi.Config.UserInterface.Language.on(lang => {
+    // language change
+    if (text[lang.toLocaleLowerCase()]) {
+          language = lang.toLowerCase();
+      } else {
+        language = fallbackLanguage;
+      }
+  })
+// class to handle the displays state
+export class MatrixState {
+    state: {
+        1?: Array<number>;
+        2?: Array<number>;
+        3?: Array<number>;
+        4?: Array<number>;
+        5?: Array<number>;
+    };
+    constructor(initialState = {}) {
+        this.state = initialState;
+    }
+    getInputOutputs(input: number): Array<number> {
+        if (!this.state[input]) {
+            return []
+        }
+        return this.state[input];
+    }
+
+    setInputOutput(input: number, output: number) {
+        if (!this.state[input]) {
+            this.clearInputState(input)
+        }
+        this.state[input].push(output);
+    }
+    getFullState() {
+        return this.state
+    }
+
+    clearState() {
+        Object.keys(this.state).forEach((key) => {
+            this.clearInputState(parseInt(key))
+        })
+    }
+
+    clearInputState(input: number) {
+        this.state[input] = []
+    }
+
+    toString() {
+        return JSON.stringify(this.state);
+    }
+
+}
+let state = new MatrixState();
+
+export function presentationPreviewStartEvent (previewStartedEvent) {
+    if(previewStartedEvent.Cause !== 'userRequested') { return } 
+    const inputConnectorId = parseInt(previewStartedEvent.LocalSource);
+    
+    xapi.Command.Presentation.Stop({ PresentationSource: inputConnectorId });
+    
+    // get the name of the outputs to create a options select the display or stop previewing
+    xapi.Status.Video.Output.Connector.get().then((connectors) => {
+        // we create a propmt so the user can select to which display they want to output.
+        // we also add a option so we can stop the previews for the inputs
+        const promptId = "ShareToScreen" + Date.now().toString(16);
+        const {promptOptions, optionIndex} = generatePromptOptions(connectors, promptId) 
+        // show the prompt
+        xapi.Command.UserInterface.Message.Prompt.Display(promptOptions);
+        const promptFeedbackEvent = xapi.Event.UserInterface.Message.Prompt.Response.on((promptResponse) => {
+
+            // if the id for the feedback does not match we ignore the option
+            if(promptResponse.FeedbackId === promptId) {
+                // stop the feedbackEvent to prevent a memory leak
+                promptFeedbackEvent();
+            
+                // if the id of the option matches the id to stop the previews we stop all previews
+                if (promptResponse.OptionId === optionIndex.toString()) {
+                    stopPreview(inputConnectorId)
+                } else {
+                    const outputConnector = connectors.find((connector) => {
+                        return connector.ConnectedDevice.Name === promptOptions[`Option.${promptResponse.OptionId}`]
+                    })
+
+                    startPreview(inputConnectorId, parseInt(outputConnector.id))
+
+                    if (outputConnector.MonitorRole === 'First') {
+                        sendMessage(text[language].shareToPrimaryScreenWarning);
+                    }
+                }
+            }
+            
+        })
+    })
+}
+// send a message to the user.
+export function sendMessage(text: string) {
+    xapi.Command.UserInterface.Message.TextLine.display({
+        Text: text,
+        Duration: 3,
+        X: 10000,
+        Y: 1100
+    })
+}
+// start the preview of the input at the selected display
+export function startPreview(inputConnectorId: number, outputConnectorId: number) {
+    xapi.Command.Video.Matrix.Assign({Output: outputConnectorId, SourceId: inputConnectorId});
+    state.setInputOutput(inputConnectorId, outputConnectorId);
+}
+
+export function stopPreview(inputConnectorId: number) {
+    // grab all the outputs that are displaying a specific inputs 
+    const outputs = state.getInputOutputs(inputConnectorId)
+    outputs.forEach((output) => {
+        xapi.Command.Video.Matrix.Unassign({Output: output, SourceId: inputConnectorId})
+    })
+    state.clearInputState(inputConnectorId);
+}
+
+
+export function generatePromptOptions(connectors: OutputConnectorStatus[], feedbackId) {
+const promptOptions: DisplayPrompt = {
+        FeedbackId: feedbackId,
+        Text: text[language].promptOptions.text,
+        Title: text[language].promptOptions.title
+    }
+
+    let optionIndex = 1
+    // loop over all the connectors
+    connectors.forEach((connector, index) => {
+        // if the connector is not connected or the primary display we wont include it when in a call.
+        // but we will include it when we are not in a call
+        if (connector.Connected !== 'True' || connector.MonitorRole === 'Recorder') {
+            return;
+        }
+
+        promptOptions[`Option.${optionIndex}`] = useMonitorName ? connector.ConnectedDevice.Name ??  connectorNames[index] : connectorNames[index]
+        optionIndex++
+    });
+    // add the option for canceling the preview
+    promptOptions['Option.5'] = text[language].promptOptions.option5;
+
+    return {promptOptions, optionIndex}
+}
+
+
+export function callStatusChanged(CallStatus) {
+    // we only want to change the monitors role when we are calling
+    if(CallStatus === 'Connected') {
+        xapi.Config.Video.Output.Connector.get().then((connectors: Array<OutputConnectorConfiguration>) => {
+            // we look for the monitors selected as primary and 
+            const recorderConnector = connectors.find((conn) => conn.MonitorRole === 'First');
+
+            if (recorderConnector) {
+                const recorderConnectorId = parseInt(recorderConnector.id) 
+                xapi.Command.Video.Matrix.Reset({Output: recorderConnectorId})    
+                xapi.Config.Video.Output.Connector[recorderConnectorId].MonitorRole.set('Recorder');
+            }
+        })  
+    }
+}
+
+export function callDisconnected (disconnectionEvent) {
+    xapi.Config.Video.Output.Connector.get().then((connectors: Array<OutputConnectorConfiguration>) => {
+        const recorederConnector = connectors.find((conn) => conn.MonitorRole === 'Recorder');
+        if (recorederConnector) {
+            xapi.Config.Video.Output.Connector[recorederConnector.id].MonitorRole.set('First');
+        }
+    })  
+}
+
+export function resetbuttonPressed(event) {
+    if (event.PanelId = 'reset_all_display') {
+        resetMatrix();
+        sendMessage(text[language].resetButtonMessage)
+    }
+}
+
+export function resetMatrix() {
+    for(let i=0; i < 3; i++) {
+        xapi.Command.Video.Matrix.Reset({Output: i})
+    }
+    
+    state.clearState();
+}
---
+++ xapi.d.ts
@@ -0,0 +1,218 @@
+export interface xapi {
+    Status?: {
+        Video: {
+            Output: {
+                Connector: {
+                    get(): Promise<Array<OutputConnectorStatus>>; // een api request om de huidige status op te halen 
+                    on(callback: (connnector: OutputConnectorStatus) => void): void; // een  "websocket" die veranderingen in de status geven 
+                };
+            };
+            Input: {
+                Connector: {
+                    get(): Promise<Array<InputConnectorStatus>>;
+                    on(callback: (connector: InputConnectorStatus) => void): void;
+                }
+            }
+        };
+        Call: {
+            Status: {
+                get(callback: (callStatus: CallStatusType) => void): void;
+                on(callback: (callStatus: CallStatusType) => void): void;
+            }
+        }
+    };
+    Config?: {
+        Video: {
+            Input: {
+                Connector: {
+                    get(): Promise<Array<InputConnectorConfig>>;
+                }
+            };
+            Output: {
+                Connector: any;
+            }
+        };
+        UserInterface: {
+            Language: {
+                get(): Promise<string>;
+                on(callback: (newLanguage: string) => void): void;
+            }
+        }
+    };
+    Command?: {
+        Video: {
+            Matrix: {
+                Assign(Object: {
+                    SourceId: number;
+                    Output: number;
+                }): void;
+                Reset(Options?: {
+                    Output: number
+                }):void;
+                Unassign(options: {
+                    Output: number
+                    RemoteMain?: number
+                    SourceId: number
+                }): void;
+            }
+        };
+        Presentation: {
+            Start(Object: { ConnectorId: number; }): void;
+            Stop(setting?: {instance?: number, PresentationSource?: number}): void;
+        };
+        UserInterface: {
+            Message: {
+                Prompt: {
+                    Display(options: DisplayPrompt): void
+                };
+                TextLine: {
+                    display(Options: {
+                        Duration?: number;
+                        Text: string;
+                        X?: number;
+                        Y?: number;
+                    }): void;
+                }
+            }
+        }
+    };
+    Event?: {
+        PresentationPreviewStarted: {
+            on(callback: (event: PresentationPreview) => void): () => void;
+        };
+        PresentationPreviewStopped: {
+            on(callback: (event: PresentationPreview) => void): () => void;
+        }
+        UserInterface: {
+            Message: { 
+                Prompt: { 
+                    Response:{
+                        on(callback: (event: { FeedbackId: string, OptionId: string, id: string }) => void): () => void;
+                    }
+                }
+            };
+            Extensions: {
+                Panel: {
+                    Clicked: {
+                        on(callback: (event: {
+                            PanelId: string,
+                            id: string
+                        }) => void): () => void;
+                    }
+                }
+            }
+        };
+        CallDisconnect: {
+            on(callback: (event: CallDisconnectEvent) => void): () => void;
+        };
+        Room: {
+            Reset(callback: (Event: any) => void): () => void;
+        }
+    }
+}
+
+export interface InputConnectorConfig {
+    InputSourceType: InputSourceTypeType;
+    Name: String; // max 50 char
+    PresentationSelection: PresentationSelectionType;
+    Quality: QualityType;
+    Visibility: VisibilityType;
+    CameraControll: {
+        CameraId: 1 | 2 | 3 | 4 | 5 | 6 | 7;
+        Mode: OnOffType;
+    };
+}
+
+export interface OutputConnectorStatus {
+    id: string;
+    Connected: BooleanStringType | "Unknown";
+    MonitorRole: MonitorRoleType;
+    Type: ConnectorTypeType;
+    Resolution: {
+        Height: string;         // number verzonden als string
+        Width: string;          // number verzonden als string
+        RefreshRate: string;    // number verzonden als string
+    }
+    ConnectedDevice: {
+        Name: string;
+        PreferredFormat: string;
+        screenSize: string;
+        CEC: {};
+    }
+}
+
+export interface InputConnectorStatus {
+    Connected: BooleanStringType | "Unknown";
+    SignalState: SignalStateType;
+    SourceId: String; // een getal in een string
+    Type: InputTypeType
+}
+
+export interface assignMatrix {
+    SourceId: number;
+    Output: Number;
+}
+
+export interface PresentationPreview {
+    Cause: string;
+    Instance: string;
+    LocalSource: '1' | '2' | '3' | '4' | '5'
+    id: string;
+}
+
+export interface DisplayPrompt {
+    Title: string;
+    FeedbackId: string;
+    Text: string;
+    Duration?: string;
+    "Option.1"?: string;
+    "Option.2"?: string;
+    "Option.3"?: string;
+    "Option.4"?: string;
+    "Option.5"?: string;
+}
+export interface CallDisconnectEvent {
+    CallId: string;
+    CallRate: string;
+    CallType: string;
+    CauseCode: string;
+    CauseOrigin: string;
+    CauseString:string;
+    CauseType: string;
+    CauseValue: string;
+    DisplayName: string;
+    Duration: string;
+    Encryption: string;
+    OrigCallDirection: string;
+    PeopleCountAverage: string;
+    Protocol: string;
+    RemoteURI: string;
+    RequestedURI: string;
+    id: string;
+}
+
+export interface OutputConnectorConfiguration {
+    CEC: {
+        Mode: OnOffType
+    };
+    Location: {
+        HorizontalOffset: string;
+        VerticalOffset: string;
+    };
+    MonitorRole: MonitorRoleType;
+    Resolution: string;
+    id: string
+}
+
+export type CallStatusType = "Connected" | "Connecting" | "Dialing" | "Disconnecting" | "EarlyMedia" | "Idle" | "onHold" | "Preserved" | "RemotePreserved" | "Ringing";
+export type BooleanStringType = "True" | "False";
+export type SignalStateType = "OK" | "Unstable" | "NotFound" | "Unkown" | "Unsupported";
+export type InputTypeType = "Camera" | "Composite" | "DVI" | "3G-SDI" | "HD-SDI" | "USBC-DP" | "HDMI" | "USB" | "Unknown" | "VGA" | "YC" | "YPbPr";
+export type MonitorRoleType = "First" | "InternalSetup" | "PresentationOnly" | "Recorder" | "Second" | "Third";
+export type ConnectorTypeType = "HDMI" | "DVI" | "LCD" | "Legacy" | "Internal";
+export type InputSourceTypeType = "PC" | "camera" | "document_camera" | "mediaplayer" | "other" | "whiteboard";
+export type OnOffType = "On" | "Off";
+export type PresentationSelectionType = "AutoShare" | "Desktop" | "Manual" | "OnConnect";
+export type QualityType = "Motion" | "Sharpness";
+export type VisibilityType = "Always" | "IfSignal" | "Never";
+export type OutputConnectorConfigType = {get(): Promise<Array<OutputConnectorConfiguration>>} | [{ MonitorRole(role: MonitorRoleType) }];
